8cc993c472c7e2774e00de5b07cb42257505f767
Michael Shepanski <michael.shepanski@gmail.com>
2023-03-14 20:27:50 -0400

    First checkin styles added.

---
+++ example/main.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+import sys
+from os.path import dirname, normpath
+from PySide6.QtWidgets import QApplication
+from PySide6.QtUiTools import QUiLoader
+from PySide6.QtCore import Qt, QCoreApplication
+
+ROOT = dirname(dirname(__file__))
+sys.path.append(ROOT)
+import qtinkwell  # noqa
+
+QCoreApplication.setAttribute(Qt.AA_ShareOpenGLContexts)
+QApplication.setStyle('default')
+
+
+if __name__ == "__main__":
+    app = QApplication()
+    window = QUiLoader().load(normpath(f'{ROOT}/example/main.ui'))
+    qtinkwell.applyStyleSheet(window)
+    window.show()
+    app.exec()
